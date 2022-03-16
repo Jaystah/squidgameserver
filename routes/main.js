0f82a485e42da5b7bcca9bd1ceb7d876008c0f24
Jay <doerga1@gmail.com>
2022-03-16 20:16:46 +0100

    change test message

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'jay is homo' })
+  res.json({ message: 'Test message' })
 })
 
 module.exports = router;
