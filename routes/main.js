3f8bc3b3d4e9b880bcae2add5713f214b899fa32
Jay <doerga1@gmail.com>
2022-03-18 00:56:20 +0100

    body

--- routes/main.js
+++ routes/main.js
@@ -9,4 +9,8 @@ router.get('/users', (req, res) => {
   res.json({usernames: ['Jay', 'Elif', 'Jazz', 'Zefan']});
 })
 
+router.post('/randompost', (req, res) => {
+  console.log(req.body);
+  res.json({message: 'Received'});
+})
 module.exports = router;
